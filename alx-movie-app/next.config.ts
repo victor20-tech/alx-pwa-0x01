5e4c5462e3c0e4ea476d21e068c2b947ad1ba9f3
victor20-tech <victoradomako10@gmail.com>
2025-10-23 09:16:20 +0000

    Updated

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -3,22 +3,28 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true
+  register: true,
 });
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "m.media-amazon.com",
-          pathname: "/**",
-          port: "",
-        }
-      ]
-    }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  headers: async () => [
+    {
+      source: "/manifest.json",
+      headers: [
+        { key: "Cache-Control", value: "public, max-age=3600, immutable" },
+      ],
+    },
+  ],
 };
 
-// cast to any to satisfy NextConfig typing for remotePatterns
 export default withPWA(nextConfig as any);
